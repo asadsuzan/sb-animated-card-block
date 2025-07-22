a853793b1ba199ee46a03005ab527fdb2630d83f
suzan <asadsuzan7@gmail.com>
2025-07-22 11:58:54 +0600

    feat: define card styles in block json

--- build/blocks-manifest.php
+++ build/blocks-manifest.php
@@ -43,7 +43,168 @@ return array(
 			'styles' => array(
 				'type' => 'object',
 				'default' => array(
-					
+					'shapeBox' => array(
+						'display' => 'inline-block',
+						'position' => 'relative',
+						'zIndex' => 1,
+						'maxWidth' => '500px',
+						'height' => '430px',
+						'margin' => array(
+							'top' => '30px',
+							'bottom' => '30px',
+							'left' => '10px',
+							'right' => '10px'
+						),
+						'boxShadow' => ' 0 6px 30px 0 rgba(0,0,0, .12)',
+						'overflow' => 'hidden',
+						'width' => '23.333%'
+					),
+					'shapeBoxHalf' => array(
+						'overflow' => 'hidden',
+						'textAlign' => 'left'
+					),
+					'shapeBoxHalfBefore' => array(
+						'content' => '',
+						'position' => 'absolute',
+						'top' => 0,
+						'left' => 0,
+						'right' => 0,
+						'bottom' => 0,
+						'transform' => 'skewY(53.5deg)',
+						'transformOrigin' => 'top left',
+						'transition' => '	ransform .4s',
+						'background' => '#fff',
+						'zIndex' => 1
+					),
+					'shapeBoxImg' => array(
+						'width' => '100%',
+						'height' => '100%',
+						'oObjectFit' => 'cover',
+						'objectFit' => 'cover'
+					),
+					'bgBlack' => array(
+						'bgColor' => '#000'
+					),
+					'ShapeBoxHalfFigCaption' => array(
+						'position' => 'absolute',
+						'left' => 0,
+						'right' => 0,
+						'bottom' => 0,
+						'padding' => array(
+							'top' => '0',
+							'bottom' => '30px',
+							'left' => '30px',
+							'right' => '30px'
+						),
+						'transition' => '	ransform .4s',
+						'transform' => 'translateY(100%)',
+						'zIndex' => 3
+					),
+					'ShapeBoxHalfFigCaptionShowContent' => array(
+						'position' => 'absolute',
+						'bottom' => 'calc(100% + 30px)',
+						'left' => '30px',
+						'right' => '30px',
+						'transition' => 'bottom .4s'
+					),
+					'cardNo' => array(
+						'fontSize' => '36px',
+						'color' => '#ffc107',
+						'padding' => array(
+							'top' => '0',
+							'bottom' => '0',
+							'left' => '0',
+							'right' => '0'
+						),
+						'margin' => array(
+							'top' => '10px',
+							'bottom' => '10px',
+							'left' => '0',
+							'right' => '0'
+						)
+					),
+					'cardMainTitle' => array(
+						'margin' => array(
+							'top' => '8px',
+							'bottom' => '0',
+							'left' => '0',
+							'right' => '0'
+						),
+						'fontWeight' => '700',
+						'fontSize' => '24px',
+						'textTransform' => 'uppercase',
+						'color' => '#292b2c'
+					),
+					'cardContent' => array(
+						'color' => '#9f9f9f',
+						'margin' => array(
+							'top' => '20px',
+							'bottom' => '0',
+							'left' => '0',
+							'right' => '0'
+						),
+						'lineHeight' => '22px',
+						'fontSize' => '15px'
+					),
+					'readMoreBtn' => array(
+						'border' => array(
+							'width' => '2px',
+							'color' => '#db3236',
+							'style' => 'solid'
+						),
+						'fontSize' => '14px',
+						'cursor' => 'pointer',
+						'padding' => array(
+							'top' => '10px',
+							'bottom' => '10px',
+							'left' => '20px',
+							'right' => '20px'
+						),
+						'display' => 'inline-block',
+						'textTransform' => 'uppercase',
+						'letterSpacing' => '.08em',
+						'fontWeight' => '600',
+						'position' => 'relative',
+						'-webkit-transition' => 'all 0.3s',
+						'-moz-transition' => 'all 0.3s',
+						'transition' => 'all 0.3s',
+						'background' => '#db3236',
+						'color' => '#fff',
+						'borderRadius' => '2px',
+						'margin' => array(
+							'top' => '25px',
+							'bottom' => '0',
+							'left' => '0',
+							'right' => '0'
+						),
+						'textDecoration' => 'none'
+					),
+					'readMoreBtnHover' => array(
+						'background' => 'transparent',
+						'color' => '#db3236'
+					),
+					'shapeBoxHalfAfter' => array(
+						'position' => 'absolute',
+						'top' => 0,
+						'left' => 0,
+						'right' => 0,
+						'bottom' => 0,
+						'background' => '#fdd744',
+						'opacity' => 0,
+						'transition' => 'opacity .4s;'
+					),
+					'shapeBoxHalfBeforeHover' => array(
+						'transform' => 'skewY(20deg)'
+					),
+					'shapeBoxHalfHoverFigCaption' => array(
+						'transform' => 'translateY(0)'
+					),
+					'shapeBoxHalfHoverFigCaptionShowContent' => array(
+						'bottom' => '100%'
+					),
+					'shapeBoxHalfHoverAfter' => array(
+						'opacity' => '1'
+					)
 				)
 			),
 			'options' => array(
--- build/sb-animated-card/index.asset.php
+++ build/sb-animated-card/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '60945c812445edb6f90c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '31d411caedffaaca07c2');
